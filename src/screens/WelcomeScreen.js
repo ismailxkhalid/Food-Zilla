5b75d0936ae77efe00303f82119040681216aa99
ismailxkhalid <ismailkhalidx@gmail.com>
2024-07-30 08:41:45 +0300

    Add Welcome Screen with reanimated library

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,12 +1,63 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image } from "react-native";
+import React, { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
 
 export default function WelcomeScreen() {
+  const ring1padding = useSharedValue(0);
+  const ring2padding = useSharedValue(0);
+
+  useEffect(() => {
+    ring1padding.value = 0;
+    ring2padding.value = 0;
+    setTimeout(
+      () => (ring1padding.value = withSpring(ring1padding.value + wp(10))),
+      200
+    );
+    setTimeout(
+      () => (ring2padding.value = withSpring(ring2padding.value + wp(8))),
+      300
+    );
+  }, []);
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-center text-3xl font-bold underline">
-        WelcomeScreen
-      </Text>
+    <View className="flex-1 items-center justify-center bg-amber-600 space-y-10">
+      <StatusBar style="light" />
+      {/* Logo image with ring Animations */}
+      <Animated.View
+        className="bg-white/20 rounded-full"
+        style={{ padding: ring1padding }}
+      >
+        <Animated.View
+          className="bg-white/20 rounded-full "
+          style={{ padding: ring2padding }}
+        >
+          <Image
+            source={require("../../assets/foodLogo.png")}
+            className=" rounded-full object-cover"
+            style={{ width: wp(60), height: hp(28) }}
+          />
+        </Animated.View>
+      </Animated.View>
+
+      {/* Welcome Text */}
+      <View className="space-y-2 flex items-center">
+        <Text
+          style={{ fontSize: hp(5) }}
+          className="text-white font-bold tracking-widest "
+        >
+          Food Zilla
+        </Text>
+        <Text
+          style={{ fontSize: wp(4) }}
+          className="text-white font-medium tracking-widest"
+        >
+          Where Every Recipe Tells a Story
+        </Text>
+      </View>
     </View>
   );
 }
